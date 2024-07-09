51732265d79ce437400d41207434dc6f4b7bd001
MatthewKelseyRT <matthew.kelsey@rising-tide.ai>
2024-07-09 14:22:49 +0200

    optimise

--- 01_cprofile/game_of_chance.py
+++ 01_cprofile/game_of_chance.py
@@ -1,34 +1,32 @@
 import random
 import time
 
-def roll_dice():
-    return random.randint(1, 6)
 
-def draw_card():
-    return random.choice(['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'])
-
-def calculate_score(dice, card):
-    if card in ['J', 'Q', 'K']:
+def calculate_score():
+    if (
+        card := random.choice(
+            ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        )
+    ) in ["J", "Q", "K"]:
         card = 10
-    elif card == 'A':
+    elif card == "A":
         card = 11
     else:
         card = int(card)
 
-    return dice + card
+    return random.randint(1, 6) + card
+
 
 def play_game():
-    dice = roll_dice()
-    card = draw_card()
-    score = calculate_score(dice, card)
-    return score
+    return calculate_score()
+
 
 def simulate_games(n):
     scores = []
     for _ in range(n):
-        score = play_game()
-        scores.append(score)
+        scores.append(play_game())
     return scores
+
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
